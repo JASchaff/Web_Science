398710d39101c0f0c35de0de28bae819f179ff69
Justin <jscha002@odu.edu>
2017-05-01 14:13:16 -0400

    ODU_CS_532_Web_Science

--- Document_Classification/fishermethod.py
+++ Document_Classification/fishermethod.py
@@ -124,8 +124,7 @@ def myclassify(articles, path, classifier):
 def getwords(doc):
     splitter=re.compile('\\W*')
     # Split the words by non-alpha characters
-    words=[s.lower( ) for s in splitter.split(doc)
-if len(s)>2 and len(s)<20]
+    words=[s.lower( ) for s in splitter.split(doc) if len(s)>2 and len(s)<20]
     # Return the unique set of words only
     return dict([(w,1) for w in words])
 
